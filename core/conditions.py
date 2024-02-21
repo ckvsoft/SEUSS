a6782be7893d00383a3a637933e17c961617ff50
ckvsoft home <chris@ckvsoft.at>
2024-02-21 19:20:43 +0100

    fix typo

--- core/conditions.py
+++ core/conditions.py
@@ -148,13 +148,13 @@ class Conditions:
     def add_abort_conditions(self):
         # Abbruchbedingungen für das Laden
         charging_abort_conditions = {
-            "Soc greater_than needed Soc": lambda: self.solardata.soc is not None and self.solardata.need_soc is not None and self.solardata.soc > self.solardata.need_soc if self.config.config_data.get('use_solar_forecast_to_abort') else False,
+            "Soc greater than needed Soc": lambda: self.solardata.soc is not None and self.solardata.need_soc is not None and self.solardata.soc > self.solardata.need_soc if self.config.config_data.get('use_solar_forecast_to_abort') else False,
             # Weitere Abbruchbedingungen hinzufügen, falls vorhanden
         }
 
         discharging_abort_conditions = {}
         if self.solardata.soc is not None and self.solardata.need_soc is not None:
-            discharging_abort_conditions["Outside sun hours and soc lower_than needed_soc"] = lambda: self.solardata.outside_sun_hours() and self.solardata.soc < self.solardata.need_soc if self.config.config_data.get('use_solar_forecast_to_abort') else False
+            discharging_abort_conditions["Outside sun hours and Soc lower than needed Soc"] = lambda: self.solardata.outside_sun_hours() and self.solardata.soc < self.solardata.need_soc if self.config.config_data.get('use_solar_forecast_to_abort') else False
 
         # Fügen Sie die Abbruchbedingungen den entsprechenden Dictionarys hinzu
         self.abort_conditions_by_operation_mode["charging_abort"].update(charging_abort_conditions)
