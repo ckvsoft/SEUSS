5dc44872933fb543c91aff2ae3e257b86e10eec4
ckvsoft home <chris@ckvsoft.at>
2025-02-04 22:27:16 +0100

    fix StatsManager calls

--- solar/solarbatterycalculator.py
+++ solar/solarbatterycalculator.py
@@ -39,11 +39,11 @@ class SolarBatteryCalculator:
         self.average_consumption = 0.0
         self.efficiency = 0.0
 
-        average_consumption_list = StatsManager.get_data('gridmeters', 'forward_hourly')
+        average_consumption_list = StatsManager().get_data('gridmeters', 'forward_hourly')
         if average_consumption_list is not None:
             self.average_consumption = round(average_consumption_list[0], 2)
 
-        efficiency_data = StatsManager.get_data('solar', 'efficiency')
+        efficiency_data = StatsManager().get_data('solar', 'efficiency')
         if efficiency_data is not None:
             self.efficiency = round(efficiency_data[0], 2)
 
