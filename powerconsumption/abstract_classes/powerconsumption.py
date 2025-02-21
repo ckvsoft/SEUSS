66c17130dac100ae5917842da2986fbb683cc57b
ckvsoft home <chris@ckvsoft.at>
2025-02-21 16:15:50 +0100

    fix

--- powerconsumption/abstract_classes/powerconsumption.py
+++ powerconsumption/abstract_classes/powerconsumption.py
@@ -53,7 +53,7 @@ class PowerDataHandler:
 
     def update_values(self, topic, payload):
         """Empfängt MQTT-Daten und aktualisiert Werte."""
-        value = payload.get("value")
+        value = payload.get("value", 0)
         self.checked_data[topic] = True
 
         if topic == "number_of_phases":
