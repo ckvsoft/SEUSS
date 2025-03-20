0a1b97e0d924173e531b7b6e6ca01125f831dec3
ckvsoft home <chris@ckvsoft.at>
2025-03-20 19:54:48 +0100

    fix ...

--- spotmarket/abstract_classes/item.py
+++ spotmarket/abstract_classes/item.py
@@ -26,7 +26,7 @@
 #
 
 # item.py
-from datetime import datetime, timedelta, UTC
+from datetime import datetime, timedelta, timezone
 from core.timeutilities import TimeUtilities
 from core.log import CustomLogger
 from core.utils import Utils
@@ -41,8 +41,7 @@ class Item:
         self.logger = CustomLogger()
 
     def is_expired(self, check_time=False):
-        # now = datetime.utcnow().replace(tzinfo=timezone.utc)
-        now = datetime.now(UTC)
+        now = datetime.utcnow().replace(tzinfo=timezone.utc)
         now_local = TimeUtilities.convert_utc_to_local(now, False)
         item_local = TimeUtilities.convert_utc_to_local(self.starttime, False)
 
