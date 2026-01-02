163b2f191f0b3610953a93140f9dca927956607b
chris <chris@ckvsoft.at>
2026-01-02 12:40:34 +0100

    fix: none existing custom_name

--- core/mqttclient.py
+++ core/mqttclient.py
@@ -96,6 +96,9 @@ class PvInverterResults(MqttResult):
             return 0.0
 
         custom_name = self.get_value(device_id, 'CustomName')
+        if not isinstance(custom_name, str) or not custom_name.strip():
+            return 0.0
+
         stats_manager_instance = StatsManager()
         stats_manager_instance.insert_new_daily_status_data("pvinverters", f"{custom_name}_forward_start", forward)
         forward_start = stats_manager_instance.get_data("pvinverters", f"{custom_name}_forward_start")
